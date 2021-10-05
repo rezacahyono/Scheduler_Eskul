20be1debfb4f843fcda581162d1b3c31037172b2
rezacahyono <rezacahyono1@gmail.com>
2021-10-05 13:32:55 +0700

    finish

--- app/src/main/java/com/rcyono/schedulereskul/ui/favorite/FavoriteFragment.java
+++ app/src/main/java/com/rcyono/schedulereskul/ui/favorite/FavoriteFragment.java
@@ -1,7 +1,6 @@
 package com.rcyono.schedulereskul.ui.favorite;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
